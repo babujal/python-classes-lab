3c281d032984a7b5503984a098e164e40d6839b4
babujal <dercamar1@gmail.com>
2024-11-24 20:10:03 -0500

    First commit

---
+++ exercises.py
@@ -0,0 +1,31 @@
+class Game():
+    def __init__(self ):
+        self.turn = 'X'
+        self.tie = False
+        self.winner = None
+        self.board = {
+            'a1': None, 'b1': None, 'c1': None,
+            'a2': None, 'b2': None, 'c2': None,
+            'a3': None, 'b3': None, 'c3': None,
+        }
+
+    def play_game(self):
+        print('Welcome message')
+
+
+    def print_board(self):
+        b = self.board
+        print(f"""
+            A   B   C
+        1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+            ----------
+        2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+            ----------
+        3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        """)
+# Define a render        
+
+game_instance = Game()
+game_instance.play_game()
+game_instance.print_board()
+
