1285a3e4785a43a5d3dc57d1c914ee98e0eca538
babujal <dercamar1@gmail.com>
2024-11-28 11:21:49 -0500

    matching print board mth

--- exercises.py
+++ exercises.py
@@ -8,6 +8,7 @@ class Game():
             'a2': None, 'b2': None, 'c2': None,
             'a3': None, 'b3': None, 'c3': None,
         }
+        self.player_turn = 'x'
 
     def play_game(self):
         print('Welcome message')
@@ -17,11 +18,11 @@ class Game():
         b = self.board
         print(f"""
             A   B   C
-        1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+        1)  {b['a1'] or None} | {b['b1'] or None} | {b['c1'] or None}
             ----------
-        2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+        2)  {b['a2'] or None} | {b['b2'] or None} | {b['c2'] or None}
             ----------
-        3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        3)  {b['a3'] or None} | {b['b3'] or None} | {b['c3'] or None}
         """)
 # Define a render        
 
